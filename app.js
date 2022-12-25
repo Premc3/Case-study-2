6a60d3f3f706d4e49e3b52e42f8ae280e825db58
Premc3 <117389287+Premc3@users.noreply.github.com>
2022-12-25 17:36:42 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,128 +1,93 @@
 // Task1: initiate app and run server at 3000
 
-// const path=require('path');
-// app.use(express.static(path.join(__dirname+'/dist/FrontEnd')));
-var Express=require("express");
-var Bodyparser = require("body-parser");
+const Express= require ("express");
+var Bodyparser =require("body-parser");
 var Mongoose = require("mongoose");
-var Cors = require("cors");
-const{urlencoded}= require("body-parser")
-const{EmployeeModel} = require('./model/employeelist.js');
-
-var app= new Express();
+const Cors = require("cors");
+const{urlencoded} = require("body-parser")
+const {EmployeeModel}=require("./model/employee");
+var app=new Express();
 app.use(Bodyparser.json());
 app.use(Bodyparser.urlencoded({extended:false}));
+app.use(Cors());
+const path=require('path');
+app.use(Express.static(path.join(__dirname+'/dist/FrontEnd')));
+// const { default: mongoose } = require("mongoose");
+
 app.listen(3000,()=>{
-    console.log("Server is up");
+    console.log('Server is up');
 })
+
 
 // Task2: create mongoDB connection 
-Mongoose.connect("mongodb+srv://Premc:12348765@cluster0.glgxktq.mongodb.net/EmployeeDB?retryWrites=true&w=majority",{useNewUrlParser:true} 
-);
+Mongoose.connect("mongodb+srv://Premc:12348765@cluster0.glgxktq.mongodb.net/EmployeeDB?retryWrites=true&w=majority",{useNewUrlParser:true})
 
 //Task 2 : write api with error handling and appropriate api mentioned in the TODO below
-app.get('/',(req,res)=>{
-    res.send('welcome')
+app.post('/',async(req,res)=>{
+    var data =req.body;
+    try{
+        const Employee = await EmployeeModel.create(data);
+        res.json(Employee)
+    } catch(error){
+        res.status(500).send(error)
+    }
 })
-app.post('/api/employeelist',async(req,res)=>{
-    var data = req.body
-    var employee = new EmployeeModel(data);
-    await student.save(
-        (err,data)=> {if(err){
-                        res.json({"Status":"Error","Error":err})
-                    }
-                        else{
-                            res.json({"Status":"Success","Data":data})
-                        }
-        }         
-            )  
-    console.log(data)
-    
+//TODO: get data from db  using api '/api/employeelist'
+app.get('/api/employeelist',async(req,res)=>{
+    try{
+        const posts = await EmployeeModel.find();
+        res.json(posts)
+    }catch(error){
+        res.status(500).send(error)
+    }
 })
 
-//TODO: get data from db  using api '/api/employeelist'
-app.get('/api/employeelist',(req,res)=>{
-    EmployeeModel.find(
-    (err,data)=>{
-        if(err){
-            res.json({"Status":"Error","Error":err})
-        }
-            else{
-                res.json({"Status":"Success","Data":data})
-            }
-        }
-    
-)
-})
 
 //TODO: get single data from db  using api '/api/employeelist/:id'
-app.get('/api/employeelist/:id',(req,res)=>{
-    let id= req.params.id
-    var data = req.body
-     EmployeeModel.find(data({id:""}),
-        (err,data)=>{
-            if(err){
-                res.json({"Status":"Error","Error":err})
-            }
-                else{
-                    res.json({"Status":"Success","Data":data})
-                }
-            }
-        
-    )
-});
-// //TODO: send data from db using api '/api/employeelist'
-// //Request body format:{name:'',location:'',position:'',salary:''}
-app.post('/api/employeelist',(req,res)=>{
-    var employeeId =req.body.employeeId;
-    var data = req.body;
-        EmployeeModel.updateMany(
-        {
-            "name":employeeName
-        },data,(err,data)=>{if (err) {
-            res.json({"Status":"Error","Error":err})
-            
-        } else {
-           res.json({"Status":"Updated","Data":data}) 
-        }}
-    )
+app.get( '/api/employeelist/:id',async(req,res)=>{
+    const{id}=req.params;
+    try{
+        const post = await EmployeeModel.findById(id);
+        res.json(post);
+    }catch(error){
+        res.status(500).send(error)  
     }
-    )
-
-// //TODO: delete a employee data from db by using api '/api/employeelist/:id'
-
-app.delete('/api/employeelist/:id',(req,res)=>{
-    var employeeId =req.body.employeeID;
-    var data = req.body;
-    let id= req.params.id;
-    EmployeeModel.findOneAndDelete(
-    {
-        "employeeId":employeeID
-    },data,(err,data)=>{if (err) {
-        res.json({"Status":"Error","Error":err})
-        
-    } else {
-       res.json({"Status":"Updated","Data":data}) 
-    }}
-)
-  
+});
+//TODO: send data from db using api '/api/employeelist'
+//Request body format:{name:'',location:'',position:'',salary:''}
+app.post('/api/employeelist',async(req,res)=>{
+    var data =req.body;
+    try{
+        const Employee = await EmployeeModel.create(data);
+        res.json(Employee)
+    } catch(error){
+        res.status(500).send(error)
+    }
 })
-// //TODO: Update  a employee data from db by using api '/api/employeelist'
-// //Request body format:{name:'',location:'',position:'',salary:''}
-app.post('/api/employeelist',(req,res)=>{
-    var employeeId =req.body.employeeId;
-    var data = req.body;
-        EmployeeModel.findOneAndUpdate(
-        {
-            "employeeId":employeeID
-        },data,(err,data)=>{if (err) {
-            res.json({"Status":"Error","Error":err})
-            
-        } else {
-           res.json({"Status":"Updated","Data":data}) 
-        }}
-    )
-    })
+
+//TODO: delete a employee data from db by using api '/api/employeelist/:id'
+app.delete('/api/employeelist/:id',async(req,res)=>{
+    const{id}=req.params;
+    try{
+        const post = await EmployeeModel.findByIdAndDelete(id);
+        res.json('Deleted Sucessfully');
+    }catch(error){
+        res.status(500).send(error)
+    }
+})
+//TODO: Update  a employee data from db by using api '/api/employeelist'
+//Request body format:{name:'',location:'',position:'',salary:''}
+app.put('/api/employeelist/:id',async(req,res)=>{
+    const {id}=req.params;
+    const data = req.body;
+    try{
+        const post = await EmployeeModel.findByIdAndUpdate(id,data);   
+        res.json('Updated Successfully');
+    }catch(error){
+        res.status(500).send(error)
+    }
+})
+
 
 //! dont delete this code. it connects the front end file.
 app.get('/*', function (req, res) {
